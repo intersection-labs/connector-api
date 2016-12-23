2afe0d267cd8e7e68d37fc552239688a566a36b1
Carlos Silva <carlos@connector.im>
2016-12-23 10:55:26 +0000

    Added support for all HTTP methods.

--- src/main/java/io/unequal/reuse/http/Endpoint.java
+++ src/main/java/io/unequal/reuse/http/Endpoint.java
@@ -9,4 +9,16 @@ public abstract class Endpoint {
 	public void post(Request req, Response resp) throws Exception {
 		throw new MethodNotAllowedException(Request.HttpMethod.POST);
 	}
+
+	public void delete(Request req, Response resp) throws Exception {
+		throw new MethodNotAllowedException(Request.HttpMethod.DELETE);
+	}
+
+	public void put(Request req, Response resp) throws Exception {
+		throw new MethodNotAllowedException(Request.HttpMethod.PUT);
+	}
+
+	public void trace(Request req, Response resp) throws Exception {
+		throw new MethodNotAllowedException(Request.HttpMethod.TRACE);
+	}
 }
--- src/main/java/io/unequal/reuse/http/EndpointServlet.java
+++ src/main/java/io/unequal/reuse/http/EndpointServlet.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import io.unequal.reuse.util.Reflection;
 import io.unequal.reuse.util.IntegrityException;
 import io.unequal.reuse.http.Request.HttpMethod;
 
@@ -10,11 +11,27 @@ import io.unequal.reuse.http.Request.HttpMethod;
 class EndpointServlet extends HttpServlet {
 
 	private final Endpoint _e;
+	private final boolean _doesGet;
+	private final boolean _doesPost;
 	
 	public EndpointServlet(Endpoint e) {
 		_e = e;
+		_doesGet = Reflection.getDeclaredMethod(true, e.getClass(), "get", Request.class, Response.class) != null;
+		_doesPost = Reflection.getDeclaredMethod(true, e.getClass(), "post", Request.class, Response.class) != null;
 	}
-	
+
+	public void doOptions(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("OPTIONS, HEAD");
+		if(_doesGet) {
+			sb.append(", GET");
+		}
+		if(_doesPost) {
+			sb.append(", POST");
+		}
+		resp.setHeader("Allow", sb.toString());
+	}
+
 	public void doHead(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		_handle(HttpMethod.GET, req, new HeadResponse(resp));
 	}
@@ -25,6 +42,18 @@ class EndpointServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		_handle(HttpMethod.POST, req, resp);
+	}
+	
+	public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		_handle(HttpMethod.DELETE, req, resp);
+	}
+	
+	public void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		_handle(HttpMethod.PUT, req, resp);
+	}
+
+	public void doTrace(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		_handle(HttpMethod.TRACE, req, resp);
 	}
 
 	private void _handle(HttpMethod method, HttpServletRequest httpReq, HttpServletResponse httpResp) throws IOException {
@@ -37,6 +66,15 @@ class EndpointServlet extends HttpServlet {
 			}
 			else if(method == HttpMethod.POST) {
 				_e.post(req, resp);
+			}
+			else if(method == HttpMethod.DELETE) {
+				_e.delete(req, resp);
+			}
+			else if(method == HttpMethod.PUT) {
+				_e.put(req, resp);
+			}
+			else if(method == HttpMethod.TRACE) {
+				_e.trace(req, resp);
 			}
 			else {
 				throw new IntegrityException(method);
